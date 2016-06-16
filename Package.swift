a6967e630542605b06b20f677d4615c9e9e4222e
Jeremi Kaczmarczyk <jeremi@tooploox.com>
2016-06-16 11:08:02 +0200

    Update SwiftyJSON to 9.x.x version

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     name: "Kitura",
         dependencies: [
             .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 17),
-            .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 8),
+            .Package(url: "https://github.com/zmeyc/SwiftyJSON.git", majorVersion: 9),
             .Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 13)
         ]
 )
